e6ca31520564c0065d8d9508adb1cfab3384d5b4
Rodrigo Nicola <rodrigo.nicola0@gmail.com>
2024-04-29 14:49:44 -0300

    Adiciona config. latex

--- Material/docusaurus.config.js
+++ Material/docusaurus.config.js
@@ -5,6 +5,8 @@
 // See: https://docusaurus.io/docs/api/docusaurus-config
 
 import {themes as prismThemes} from 'prism-react-renderer';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 /** Global variables **/
 const organizationName = 'rmnicola'; // Usually your GitHub org/user name.
@@ -46,6 +48,8 @@ const config = {
         docs: {
           sidebarPath: './sidebars.js',
           routeBasePath: '/',
+          remarkPlugins: [remarkMath],
+          rehypePlugins: [rehypeKatex],
         },
         blog: false,
         theme: {
@@ -53,6 +57,15 @@ const config = {
         },
       }),
     ],
+  ],
+      stylesheets: [
+    {
+      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
+      type: 'text/css',
+      integrity:
+        'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
+      crossorigin: 'anonymous',
+    },
   ],
 
   themeConfig:
